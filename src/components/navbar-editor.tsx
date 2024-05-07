a1746eb77d514fb5fa86133501652a571f492eaf
graham <graham@hackclub.com>
2024-05-07 10:30:21 -0400

    Reskinning stuck to bug reporting (#1649)
    * Reskinning stuck to bug reporting
    
    * rename stuck to bug report
    
    * Do away w/ stuckcategory
    
    * revert
    
    * Switch to bug category
    
    * typo
    
    * ...
    
    * add debug logging on failure to update airtable
    
    * Update navbar-editor.tsx
    
    forcing recompile

--- src/components/navbar-editor.tsx
+++ src/components/navbar-editor.tsx
@@ -85,8 +85,7 @@ interface EditorNavbarProps {
 	roomParticipants: string[];
 }
 
-type StuckCategory = "Logic Error" | "Syntax Error" | "Other";
-
+type StuckCategory = "Logic Error" | "Syntax Error" | "Other" | "UI" | "Code Compilation" | "Bitmap Editor" | "Tune Editor" | "Help/Tutorial Window" | "AI Chat" | "Website";
 type StuckData = {
 	category: StuckCategory;
 	description: string;
@@ -437,144 +436,74 @@ export default function EditorNavbar(props: EditorNavbarProps) {
 								...stuckData.value,
 							};
 
-							try {
-								const response = await fetch(
-									"/api/stuck-request",
-									{
-										method: "POST",
-										body: JSON.stringify(payload),
-									}
-								);
-								// Let the user know we'll get back to them after we've receive their complaint
-								if (response.ok) {
-									alert(
-										"We received your request and will get back to you via email."
-									);
-								} else
-									alert(
-										"We couldn't send your request. Please make sure you're connected and try again."
-									);
-							} catch (err) {
-								console.error(err);
-							} finally {
-								isSubmitting.value = false;
-							}
-						}}
-					>
-						<label htmlFor="issue category">
-							What is the type of issue you're facing?
-						</label>
-						<select
-							value={stuckData.value.category}
-							onChange={(event) => {
-								stuckData.value = {
-									...stuckData.value,
-									category: (
-										event.target! as HTMLSelectElement
-									).value as StuckCategory,
-								};
-							}}
-							name=""
-							id=""
-						>
-							<option value={"Logic Error"}>Logic Error</option>
-							<option value={"Syntax Error"}>Syntax Error</option>
-							<option value={"Other"}>Other</option>
-						</select>
-						<label htmlFor="Description">
-							Please describe the issue you're facing below
-						</label>
-						<Textarea
-							required
-							value={stuckData.value.description}
-							onChange={(event) => {
-								stuckData.value = {
-									...stuckData.value,
-									description: event.target.value,
-								};
-							}}
-							placeholder="Example: After 2 seconds, the browser tab suddenly freezes and I do not know why."
-						/>
-						<br />
-						<Button type="submit" disabled={isSubmitting.value}>
-							{isSubmitting.value ? "Sending..." : "Send"}
-						</Button>
-					</form>
-				</div>
-			)}
-			{showNavPopup.value && (
-				<div class={styles.navPopup}>
-					<ul>
-						{props.persistenceState.value.session?.session.full ? (
-							<>
-								<li>
-									<a href="/~">Your games</a>
-								</li>
-								<li>
-									<a href="/~/new">New game</a>
-								</li>
-							</>
-						) : (
-							<>
-								<li>
-									<a href="/~">Your games (log in)</a>
-								</li>
-								<li>
-									<a
-										href="javascript:void"
-										role="button"
-										onClick={() => {
-											if (resetState.value === "idle") {
-												resetState.value = "confirm";
-											} else {
-												codeMirror.value?.dispatch({
-													changes: {
-														from: 0,
-														to: codeMirror.value
-															.state.doc.length,
-														insert: defaultExampleCode,
-													},
-												});
-												resetState.value = "idle";
-											}
-										}}
-									>
-										{resetState.value === "idle"
-											? "Reset game code"
-											: "Are you sure?"}
-									</a>
-								</li>
-							</>
-						)}
+					try {
+						const response = await fetch("/api/bug-report", {
+							method: "POST",
+							body: JSON.stringify(payload)
+						})
+						// Let the user know we'll get back to them after we've receive their complaint
+						if (response.ok) {
+							alert("We received your bug report!  Thanks!")
+						} else alert("We couldn't send your request. Please make sure you're connected and try again.")
+
+					} catch (err) {
+						console.error(err);
+					} finally {
+						isSubmitting.value = false;
+					}
+			}}>
+					<label htmlFor="issue category">What is the type of issue you're facing?</label>
+					<select value={stuckData.value.category} onChange={(event) => {
+						stuckData.value = { ...stuckData.value, category: (event.target! as HTMLSelectElement).value as StuckCategory }
+					}} name="" id="">
+						<option value={"Other"}>Other</option>
+						<option value={"UI"}>UI</option>
+						<option value={"Code Compilation"}>Code Compilation</option>
+						<option value={"Bitmap Editor"}>Bitmap Editor</option>
+						<option value={"Tune Editor"}>Tune Editor</option>
+						<option value={"Help/Tutorial Window"}>Help/Tutorial Window</option>
+						<option value={"AI Chat"}>AI Chat</option>
+						<option value={"Website"}>Website</option>
+					</select>
+					<label htmlFor="Description">Please describe the issue you're facing below</label>
+					<Textarea required value={stuckData.value.description} onChange={event => {
+						stuckData.value = { ...stuckData.value, description: event.target.value }
+					}} placeholder='Example: After 2 seconds, the browser tab suddenly freezes and I do not know why.' />
+					<br />
+					<Button type='submit' disabled={isSubmitting.value}>
+						{isSubmitting.value ? "Sending..." : "Send"}
+					</Button>
+				</form>
+			</div>
+		)}
+		{showNavPopup.value && <div class={styles.navPopup}>
+			<ul>
+				{props.persistenceState.value.session?.session.full
+					? (<>
+						<li><a href='/~'>Your games</a></li>
+						<li><a href='/~/new'>New game</a></li>
+					</>)
+					: (<>
+						<li><a href='/~'>Your games (log in)</a></li>
 						<li>
-							{props.roomId === null ? (
-								<a
-									href="javascript:void"
-									role="button"
-									onClick={() => {
-										showRoomPopup.value = true;
-										props.setIsInRoom(true);
-									}}
-								>
-									Start a Room
-								</a>
-							) : (
-								<a
-									href="javascript:void"
-									role="button"
-									onClick={() => {
-										showRoomPopup.value = true;
-									}}
-								>
-									Room Details
-								</a>
-							)}
-						</li>
-						<li>
-							<a href="/gallery">Gallery</a>
-						</li>
-						<li>
-							<a href="/get">Get a Sprig</a>
+							<a href='javascript:void' role='button' onClick={() => {
+								if (resetState.value === 'idle') {
+									resetState.value = 'confirm'
+								} else {
+									codeMirror.value?.dispatch({
+										changes: {
+											from: 0,
+											to: codeMirror.value.state.doc.length,
+											insert: defaultExampleCode
+										}
+									})
+									resetState.value = 'idle'
+								}
+							}}>
+								{resetState.value === 'idle'
+									? 'Reset game code'
+									: 'Are you sure?'}
+							</a>
 						</li>
 					</ul>
 					<div class={styles.divider} />
